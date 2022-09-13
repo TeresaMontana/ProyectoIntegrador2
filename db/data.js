9a0c0026b16ca4ef483a667742a9ee30e15c9ac8
lulicabello <lcabellomartinonis@udesa.edu.ar>
2022-09-13 15:41:40 -0300

    Cambios Lucila

--- db/data.js
+++ db/data.js
@@ -1,5 +1,41 @@
 const data = {
-    listadoUsuario : [],
+    listadoUsuario : [
+     {
+        username: 'manuelcilfone'
+        foto:'',
+        email: 'manucilfone@hotmail.com',
+        password:'1234',
+        fecha: '2001-1-5',
+        dni: '42385967',
+        posteos:10,
+        seguidores:80,
+        seguidos:90
+    },
+    {
+        username: 'felipecifre'
+        foto:'',
+        email: 'felipecifre@hotmail.com',
+        password:'15u5u4i',
+        fecha: '2005-1-5',
+        dni: '42385967',
+        posteos:6,
+        seguidores:40,
+        seguidos:100
+    },
+    {
+        username: 'juanalucini'
+        foto:'',
+        email: 'juanilucini@hotmail.com',
+        password:'984838',
+        fecha: '1999-1-5',
+        dni: '42385967',
+        posteos:8,
+        seguidores:200,
+        seguidos:240
+    },
+
+
+    ],
     
     listadoPosteos : [
         {
