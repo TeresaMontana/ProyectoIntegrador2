bb99135815cf5f0b3229db82d7e95596524ce541
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2022-09-18 20:15:58 -0300

    Correccion Lucila

--- db/data.js
+++ db/data.js
@@ -1,75 +1,86 @@
 const data = {
     listadoUsuario : [
-
-     {
-        id:1,
-        email:' manu@cilfone.com',
-        username: 'manuelcilfone',
-        foto:'/img/FotoPerfil/hombre1.jpeg',
-        password:1234,
-        nacimiento: '1999-1-5',
-        posteos:10,
-        seguidores:80,
-        seguidos:90,
-        dni: 418181
-    },
-    {
-        id:2,
-        email:' manu@cilfone.com',
-        username: 'felipecifre',
-        foto:'/img/FotoPerfil/hombre2.jpeg',
-        password:1234,
-        nacimiento: '1999-1-5',
-        posteos:6,
-        seguidores:40,
-        seguidos:100,
-        dni: 11111
-    },
-    {
-        id:3,
-        email:' manu@cilfone.com',
-        username: 'juanalucini',
-        foto:'/img/FotoPerfil/mujer1.jpeg',
-        password:1234,
-        nacimiento: '1999-1-5',
-        posteos:8,
-        seguidores:200,
-        seguidos:240,
-        dni: 22222
-    },
-    {
-        id:4,
-        email:' manu@cilfone.com',
-        username: 'carolina11',
-        foto:'/img/FotoPerfil/mujer2.jpeg',
-        password:1234,
-        nacimiento: '1999-1-5',
-        posteos:8,
-        seguidores:200,
-        seguidos:240,
-        dni: 33333
-    },
-    {
-        id:5,
-        email:' manu@cilfone.com',
-        username: 'solchioccarelli',
-        foto:'/img/FotoPerfil/mujer3.jpeg',
-        password:1234,
-        nacimiento: '1999-1-5',
-        posteos:4,
-        seguidores:200,
-        seguidos:240,
-        dni: 44444
-    }
+        {
+            id:1,
+            email:' manu@cilfone.com',
+            username: 'manuelcilfone',
+            foto:'/img/FotoPerfil/hombre1.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:10,
+            seguidores:80,
+            seguidos:90,
+            dni: 418181
+        },
+        {
+            id:2,
+            email:' manu@cilfone.com',
+            username: 'felipecifre',
+            foto:'/img/FotoPerfil/hombre2.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:6,
+            seguidores:40,
+            seguidos:100,
+            dni: 11111
+        },
+        {
+            id:3,
+            email:' manu@cilfone.com',
+            username: 'juanalucini',
+            foto:'/img/FotoPerfil/mujer1.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:8,
+            seguidores:200,
+            seguidos:240,
+            dni: 22222
+        },
+        {
+            id:4,
+            email:' manu@cilfone.com',
+            username: 'carolina11',
+            foto:'/img/FotoPerfil/mujer2.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:8,
+            seguidores:200,
+            seguidos:240,
+            dni: 33333
+        },
+        {
+            id:5,
+            email:' manu@cilfone.com',
+            username: 'solchioccarelli',
+            foto:'/img/FotoPerfil/mujer3.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:4,
+            seguidores:200,
+            seguidos:240,
+            dni: 44444
+        },
+        {
+            id:5,
+            email:' manu@cilfone.com',
+            username: 'solchioccarelli',
+            foto:'/img/FotoPerfil/mujer3.jpeg',
+            password:1234,
+            nacimiento: '1999-1-5',
+            posteos:4,
+            seguidores:200,
+            seguidos:240,
+            dni: 12344
+        }
     ],
     
-         listadoPosteos : [
-      { 
-        PostId: 1,
-        foto: '/img/FotoProductos/mujer/foto1.jpeg',
-        texto: 'Anillos hombre',
-        dni: 44444,
-    },
+    listadoPosteos : [
+        { 
+            PostId: 1,
+            foto: '/img/FotoProductos/mujer/foto1.jpeg',
+            texto: 'Anillos hombre',
+            dni: 44444,
+        },
         {
             PostId: 2,
             dni: 33333,
