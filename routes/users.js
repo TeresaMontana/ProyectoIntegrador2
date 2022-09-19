90417c63753b8b0bab82fe479cf371b1b89a9e0f
TeresaMontana <89273964+TeresaMontana@users.noreply.github.com>
2022-09-18 22:49:46 -0300

    arregle metodo del controlador de usuario

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
 
-/* GET users listing. */
 
 router.get('/login', usersController.login);
 router.get('/registracion', usersController.registracion);
