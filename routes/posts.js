d49a0ca67374f71ffaa284924f2e1f9f61cc397c
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2022-11-24 21:31:38 -0300

    cambios rutas

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var postsController = require ('../controllers/postsController')
+const postsController = require('../controllers/postsController');
 
-/*GET home page */
-router.get('/agregar', postsController.agregarPost);
+/* Requiero Modulos Multer y Path*/
+
+let multer = require ('multer');
+let path = require ('path');
+
+/* Config. de Multer */
+
+let storage = multer.diskStorage({
+    destination: function(req,file,cb){
+    cb(null,path.join(__dirname, '../public/img/Posteos'));
+},
+    filename: function(req,file,callback){
+    callback(null,file.fieldname + '-' + Date.now()+ path.extname(file.originalname));
+}
+});
+
+let upload = multer({storage:storage});
+
+router.get("/agregar", postsController.vista_agregarPost);
+
+router.post("/agregar", upload.single("Posteos"), postsController.store_agregarPost);
+
+router.get('/editar/:id', postsController.vista_editarPost);
+router.get("/detallePost/:id", postsController.detallePost);
+router.post("/editar/:id", upload.single("Posteos"), postsController.store_editarPost);
+router.post("/detallePost/:id", postsController.store_comentario);
 router.get("/resultadoBusqueda", postsController.busqueda);
-router.get('/detallePost/:id', postsController.detallePost);
 
-
-module.exports = router
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ let path = require ('path');
 
 let storage = multer.diskStorage({
     destination: function(req,file,cb){
-    cb(null,path.join(__dirname, '../public/img/FotoPerfil'));
+    cb(null,path.join(__dirname, '../public/img/FotodePerfil'));
 },
     filename: function(req,file,callback){
     callback(null,file.fieldname + '-' + Date.now()+ path.extname(file.originalname));
@@ -25,20 +25,17 @@ let upload = multer({storage:storage});
 
 // Renderiza la vista de login
 router.get('/login', usersController.login);
-
 // Renderiza la vista de registro
-router.get('/registracion', usersController.create);
-
+router.get('/registracion', usersController.registracion);
 // Renderiza la vista de mi perfil
 router.get('/miPerfil', usersController.miPerfil);
 router.get('/editarPerfil', usersController.editarPerfil);
 
 // Procesa el formulario de login
 router.post('/login', usersController.loginPost);
-
 // Procesa el formulario de registro
 router.post('/registracion', upload.single('FotodePerfil'), usersController.store);
-
+router.post('/registracion', usersController.store);
 // Procesa el formulario de edicion de perfil
 router.post('/editarPerfil', upload.single('FotodePerfil'), usersController.actualizarPerfil);
 router.post('/logout', usersController.logout);
